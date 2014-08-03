9f06d11b0a5f75c037d3fce4b70849500dfc9dc8
Sasa Sekulic <ja@sasasekulic.info>
2014-08-04 00:40:54 +0200

    added javadocs, getConfigForUser to HueBridgeApi

--- app/src/main/java/com/alterego/ibeaconapp/app/hue/api/HueBridgeApiManager.java
+++ app/src/main/java/com/alterego/ibeaconapp/app/hue/api/HueBridgeApiManager.java
@@ -1,5 +1,6 @@
 package com.alterego.ibeaconapp.app.hue.api;
 
+import com.alterego.ibeaconapp.app.hue.data.HueBridgeConfiguration;
 import com.alterego.ibeaconapp.app.hue.data.requests.ConnectUserRequest;
 import com.alterego.ibeaconapp.app.hue.data.responses.ConnectUserResponse;
 import com.alterego.ibeaconapp.app.interfaces.IDisposable;
@@ -32,11 +33,29 @@ public class HueBridgeApiManager implements IDisposable {
         mHueBridgeApiService = restAdapter.create(IHueBridgeApi.class);
     }
 
+    /**
+     * This call tries to register the user with the bridge. The call must be executed inside 30 seconds of the pressing
+     * the link button on the bridge
+     *
+     * @param username String of the username to be registered with the bridge
+     * @return {@link ConnectUserResponse} Response which can contain an error or a success map with params inside
+     */
     public Observable<List<ConnectUserResponse>> getUserConnected (String username) {
 
         mSettingsManager.getLogger().debug("HueBridgeApiManager getUserConnected username = " + username);
         return mHueBridgeApiService.getUserConnected(new ConnectUserRequest(username));
 
+    }
+
+    /**
+     * This call fetches bridge configuration for the last registered bridge username obtained from HueBridgeManager.
+     *
+     * @return {@link com.alterego.ibeaconapp.app.hue.data.HueBridgeConfiguration} {@link rx.Observable} Returns a bridge configuration object
+     */
+    public Observable<HueBridgeConfiguration> getConfigForUser() {
+        String username = mSettingsManager.getHueBridgeManager().getLastHueBridgeUsername();
+        mSettingsManager.getLogger().debug("HueBridgeApiManager getConfigForUser username = " + username);
+        return mHueBridgeApiService.getConfigForUser(username);
     }
 
     @Override
--- app/src/main/java/com/alterego/ibeaconapp/app/hue/api/IHueBridgeApi.java
+++ app/src/main/java/com/alterego/ibeaconapp/app/hue/api/IHueBridgeApi.java
@@ -1,16 +1,35 @@
 package com.alterego.ibeaconapp.app.hue.api;
 
+import com.alterego.ibeaconapp.app.hue.data.HueBridgeConfiguration;
 import com.alterego.ibeaconapp.app.hue.data.requests.ConnectUserRequest;
 import com.alterego.ibeaconapp.app.hue.data.responses.ConnectUserResponse;
 
 import java.util.List;
 
 import retrofit.http.Body;
+import retrofit.http.GET;
 import retrofit.http.POST;
+import retrofit.http.Path;
 import rx.Observable;
 
 public interface IHueBridgeApi {
 
+    /**
+     * This call tries to register the user with the bridge. The call must be executed inside 30 seconds of the pressing
+     * the link button on the bridge
+     *
+     * @param request {@link ConnectUserRequest} ConnectUserRequest with the username to be registered with the bridge
+     * @return {@link ConnectUserResponse} Response which can contain an error or a success map with params inside
+     */
     @POST("/api")
     Observable<List<ConnectUserResponse>> getUserConnected(@Body ConnectUserRequest request);
+
+    /**
+     * This call fetches bridge configuration
+     *
+     * @param username  Username registered with the bridge
+     * @return {@link HueBridgeConfiguration} {@link rx.Observable} Returns a bridge configuration object
+     */
+    @GET("/api/{username}/config")
+    Observable<HueBridgeConfiguration> getConfigForUser(@Path("username") String username);
 }
