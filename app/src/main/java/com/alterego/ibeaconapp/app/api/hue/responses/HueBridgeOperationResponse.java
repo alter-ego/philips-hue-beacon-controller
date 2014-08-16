c391a29ac0289c1dd3b6021d2184b5f5d10c4bd6
Sasa Sekulic <ja@sasasekulic.info>
2014-08-16 18:13:53 +0200

    added isOperationSuccessful to HueBridgeOperationResponse

--- app/src/main/java/com/alterego/ibeaconapp/app/api/hue/responses/HueBridgeOperationResponse.java
+++ app/src/main/java/com/alterego/ibeaconapp/app/api/hue/responses/HueBridgeOperationResponse.java
@@ -1,13 +1,16 @@
 package com.alterego.ibeaconapp.app.api.hue.responses;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import lombok.Getter;
 
 public class HueBridgeOperationResponse {
 
-    @Getter public Map<String, String> success = new HashMap<String, String>();
-    @Getter public Map<String, String> error = new HashMap<String, String>();
+    @Getter public Map<String, String> success;
+    @Getter public Map<String, String> error;
+
+    public boolean isOperationSuccessful () {
+        return success != null;
+    }
 
 }
