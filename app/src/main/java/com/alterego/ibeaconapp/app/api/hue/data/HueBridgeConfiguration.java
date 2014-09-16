f31b7b8ce23945ac97b591b133f20620348f734c
Sasa Sekulic <ja@sasasekulic.info>
2014-09-16 16:14:37 +0200

    updated HueBridgeConfiguration to api v1.3

--- app/src/main/java/com/alterego/ibeaconapp/app/api/hue/data/HueBridgeConfiguration.java
+++ app/src/main/java/com/alterego/ibeaconapp/app/api/hue/data/HueBridgeConfiguration.java
@@ -12,39 +12,8 @@ import lombok.ToString;
 @ToString
 public class HueBridgeConfiguration {
 
-    //Editable name given to the bridge. This is also its uPnP name, so will reflect the actual uPnP name after any conflicts have been resolved.
+    //string 4..16, Editable name given to the bridge. This is also its uPnP name, so will reflect the actual uPnP name after any conflicts have been resolved.
     @Getter @Setter public String name;
-
-    @Getter @Setter @SerializedName("ipaddress") public String IPAddress;
-
-    @Getter @Setter @SerializedName("mac") public String macAddress;
-
-    //Port of the proxy being used by the bridge. If set to 0 then a proxy is not being used.
-    @Getter @Setter @SerializedName("proxyport") public short proxyPort;
-
-    //Current time stored on the bridge.
-    @Getter @Setter @SerializedName("UTC") public String currentBridgeUTCTime;
-
-    //Software version of the bridge.
-    @Getter @Setter @SerializedName("swversion") public String currentSoftwareVersion;
-
-    //IP Address of the proxy server being used. A value of “none” indicates no proxy.
-    @Getter @Setter @SerializedName("proxyaddress") public String proxyAddress;
-
-    //Network mask of the bridge.
-    @Getter @Setter @SerializedName("netmask") public String networkMask;
-
-    //Gateway IP address of the bridge.
-    @Getter @Setter @SerializedName("gaeway") public String gatewayIP;
-
-    //Whether the IP address of the bridge is obtained with DHCP.
-    @Getter @Setter @SerializedName("dhcp") public boolean isAddressObtainedViaDHCP;
-
-    //Indicates whether the link button has been pressed within the last 30 seconds.
-    @Getter @Setter @SerializedName("linkbutton") public boolean isLinkButtonPressed;
-
-    //This indicates whether the bridge is registered to synchronize data with a portal account.
-    @Getter @Setter @SerializedName("portalservices") public boolean isBridgeRegisteredWithPortal;
 
     //Contains information related to software updates.
     @Getter @Setter @SerializedName("swupdate") public ArrayList<HueBridgeSoftwareUpdate> bridgeSoftwareUpdates;
@@ -52,5 +21,47 @@ public class HueBridgeConfiguration {
     //An array of whitelisted user IDs.
     @Getter @Setter @SerializedName("whitelist") public HashMap<String, HueBridgeWhitelistEntry> userWhitelistEntries;
 
+    //The version of the hue API in the format <major>.<minor>.<patch>, for example 1.2.1
+    @Getter @Setter @SerializedName("apiversion") public String currentApiVersion;
+
+    //Software version of the bridge.
+    @Getter @Setter @SerializedName("swversion") public String currentSoftwareVersion;
+
+    //IP Address of the proxy server being used. A value of “none” indicates no proxy.
+    @Getter @Setter @SerializedName("proxyaddress") public String proxyAddress;
+
+    //Port of the proxy being used by the bridge. If set to 0 then a proxy is not being used.
+    @Getter @Setter @SerializedName("proxyport") public short proxyPort;
+
+    //Indicates whether the link button has been pressed within the last 30 seconds.
+    @Getter @Setter @SerializedName("linkbutton") public boolean isLinkButtonPressed;
+
+    @Getter @Setter @SerializedName("ipaddress") public String IPAddress;
+
+    @Getter @Setter @SerializedName("mac") public String macAddress;
+
+    //Network mask of the bridge.
+    @Getter @Setter @SerializedName("netmask") public String networkMask;
+
+    //Gateway IP address of the bridge.
+    @Getter @Setter @SerializedName("gateway") public String gatewayIP;
+
+    //Whether the IP address of the bridge is obtained with DHCP.
+    @Getter @Setter @SerializedName("dhcp") public boolean isAddressObtainedViaDHCP;
+
+    //This indicates whether the bridge is registered to synchronize data with a portal account.
+    @Getter @Setter @SerializedName("portalservices") public boolean isBridgeRegisteredWithPortal;
+
+    //Current time stored on the bridge.
+    @Getter @Setter @SerializedName("UTC") public String currentBridgeUTCTime;
+
+    //The local time of the bridge. "none" if not available.
+    @Getter @Setter @SerializedName("localtime") public String currentBridgeLocalTime;
+
+    //Timezone of the bridge as OlsenIDs, like "Europe/Amsterdam" or "none" when not configured.
+    @Getter @Setter @SerializedName("timezone") public String currentBridgeLocalTimezone;
+
+    //The current wireless frequency channel used by the bridge. It can take values of 11, 15, 20,25 or 0 if undefined (factory new).
+    @Getter @Setter @SerializedName("zigbeechannel") public String zigbeeChannel;
 
 }
